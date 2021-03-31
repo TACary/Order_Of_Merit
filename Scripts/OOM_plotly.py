44248d4d0d12528d071329bc75a982d8177a5d69
TACary <timcarytc@gmail.com>
2021-03-30 19:48:19 -0600

    working dash script

--- Scripts/OOM_plotly.py
+++ Scripts/OOM_plotly.py
@@ -5,12 +5,36 @@ Created on Tue Mar 30 17:59:50 2021
 @author: Tim
 """
 
-import pandas as pd
-import numpy as np
 import plotly.express as px
 import plotly.figure_factory as ff
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
 
 df = pd.read_csv(r"C:\Users\Tim\Documents\Python Scripts\OOM_results.csv")
 
-fig = ff.create_table(df)
-fig.show()
+Event = df['Event'].unique()
+
+app = dash.Dash(__name__)
+server = app.server
+
+
+app.layout = html.Div([
+    html.Div([dcc.Dropdown(id='Event-select', options=[{'label': i, 'value': i} for i in Event],
+                           value='Event', style={'width': '140px'})]),
+    dcc.Graph(id='table',figure=fig)
+])
+
+@app.callback(
+    Output('table', 'figure'),
+    [Input('Event-select', 'value')]
+)
+def update_graph(event):
+    import plotly.express as px
+    import plotly.figure_factory as ff
+    return ff.create_table(df[df['Event']==event])
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
